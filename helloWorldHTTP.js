941e077ff712cd44575eee649b82f5f44c00b13e
pfores <paufores@iesebre.com>
2015-10-27 18:29:11 +0100

    Creat el helloWorldTCP.js

--- helloWorldHTTP.js
+++ helloWorldHTTP.js
@@ -3,8 +3,9 @@ var http = require('http');
 
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World\n");
+  console.log(request);
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.end("<h1>Hello World</h1>");
 });
 
 // Listen on port 8000, IP defaults to 127.0.0.1
